bb1a8cd9985c0dc1e749eb54493d7a5c85723c68
mr-stan-dev <5413083+mr-stan-dev@users.noreply.github.com>
2025-09-19 22:04:11 +0100

    Bump android app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "app.flymap"
         minSdk = 23
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.2"
+        versionCode = 9
+        versionName = "0.1.3"
         ndkVersion = "28.1.13356709"
     }
 
